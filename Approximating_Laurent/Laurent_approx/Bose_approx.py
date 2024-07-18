73dfc8361d3c2dbf630432d48741fdbfdeb75396
Benedict Blaschko <ge93kux@mytum.de>
2024-07-18 18:36:30 +0200

    coeff are more challenging that expected

--- Approximating_Laurent/Laurent_approx/Bose_approx.py
+++ Approximating_Laurent/Laurent_approx/Bose_approx.py
@@ -11,10 +11,37 @@ x, y, z = symbols('x y z')
 
 f_ref = 1/ (1-sp.exp(-x)) 
 
-def coeff(k) :
-     return 1
+a_0=1/12 
+
+coeff_list = [a_0]
+
+# coeff implements \frac{2k+1}{2(2k+3)!} - \sum_{j=0}^{k-1} \frac{a_j}{(2k+1-2j)!} 
+# implement 
+# j is here j-1 to include the first term
 
 
+def coeff(k, j) :
+     if (k==0): 
+          return a_0
+     if(j==0): 
+          return ( (2*k+1)/(2 * factorial(2*k+3)) + coeff(k, j+1))
+     
+
+     aux = coeff_list[j-1]/factorial(2*k+1 - 2*j)
+     if(j>1) : 
+          coeff_list.add()
+          print(coeff_list)
+          coeff_list[j-1] += aux 
+          print(coeff_list)
+     print(f"aux {aux}")
+     return 0
+
+print(coeff(1,0))
+
+
+# bose implementt: 1/x + \frac{1}{2} + x \sum_{k=0}^{2N-1}
+# index is here k-2 so i can get the two cases at the beginning included
+# one could also start at -2 i don't know, or have an additional param 
 def bose(N, index): 
     if(index==0):
           return 1/x + bose(N-1, index+1)
@@ -30,5 +57,5 @@ def bose(N, index):
          return aux + bose(N, index+1)
 
 
-print(bose(5,0))
+#print(bose(5,0))
 
