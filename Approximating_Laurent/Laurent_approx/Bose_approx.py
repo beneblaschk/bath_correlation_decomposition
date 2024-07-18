b1703962e250cd0c19ff7f6cce717d31d30308ec
Benedict Blaschko <ge93kux@mytum.de>
2024-07-18 18:02:51 +0200

    removed x from params, i want the full form, non evaluated

--- Approximating_Laurent/Laurent_approx/Bose_approx.py
+++ Approximating_Laurent/Laurent_approx/Bose_approx.py
@@ -9,23 +9,26 @@ x, y, z = symbols('x y z')
 
 # reference function: 
 
-f_ref = 1/ (1-exp(-x)) 
+f_ref = 1/ (1-sp.exp(-x)) 
 
 def coeff(k) :
      return 1
 
 
-def bose(x, N, index): 
+def bose(N, index): 
     if(index==0):
-          return 1/x + bose(x,N-1, index+1)
-    if(index==1)
-          return 1/2 + bose(x,N-1, index+1) 
+          return 1/x + bose(N-1, index+1)
+    if(index==1):
+          return 1/2 + bose(N-1, index+1) 
     
     # implement: x \sum_{k=0}^{2N-1}
     aux = x * (coeff(index-2) * x**(2*(index-2)))
+    print(aux)
     if(N-1==index-2):
          return aux
     else:
-         return aux + bose(x, N, index+1)
+         return aux + bose(N, index+1)
 
+
+print(bose(5,0))
 
