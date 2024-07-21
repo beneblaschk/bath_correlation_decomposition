f25cd54c8373c1c9c684b743e2411a06b486f8cf
Benedict Blaschko <ge93kux@mytum.de>
2024-07-21 15:52:51 +0200

    added coeff_list and put it into bose(N,index)

--- Approximating_Laurent/Laurent_approx/Bose_approx.py
+++ Approximating_Laurent/Laurent_approx/Bose_approx.py
@@ -17,6 +17,9 @@ x, y, z = symbols('x y z')
 
 f_ref = 1/ (1-sp.exp(-x)) 
 
+coeff_list = [0.08333333333333333, -0.0013888888888888874, 3.3068783068782915e-05, -8.267195767195603e-07, 2.0876756987866386e-08, -5.284190138685735e-10, 1.3382536530666791e-11, -3.3896802963044296e-13, 8.586062056093802e-15, -2.1748686983715528e-16, 5.509002826470406e-18, 0, 0, 0, 0]
+
+
 def coeff(k) :
      return 1
 
@@ -28,7 +31,7 @@ def bose(N, index):
           return 1/2 + bose(N-1, index+1) 
     
     # implement: x \sum_{k=0}^{2N-1}
-    aux = x * (coeff(index-2) * x**(2*(index-2)))
+    aux = x * (coeff_list[index-2] * x**(2*(index-2)))
     print(aux)
     if(N-1==index-2):
          return aux
@@ -36,5 +39,5 @@ def bose(N, index):
          return aux + bose(N, index+1)
 
 
-print(bose(5,0))
+print(bose(10,0))
 
