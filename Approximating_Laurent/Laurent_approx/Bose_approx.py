9e1deb205741c9f8251b7ee926df037f98eeac8b
Benedict Blaschko <ge93kux@mytum.de>
2024-07-21 16:21:19 +0200

    added a helper caller for coeff and sum to have less param in original call, added verbose to coeff and sum

--- Approximating_Laurent/Laurent_approx/Bose_approx.py
+++ Approximating_Laurent/Laurent_approx/Bose_approx.py
@@ -14,8 +14,8 @@ f_ref = 1/ (1-sp.exp(-x))
 
 a_0=1/12 
 
-coeff_list = [0.08333333333333333, -0.0013888888888888874, 3.3068783068782915e-05, -8.267195767195603e-07, 2.0876756987866386e-08, -5.284190138685735e-10, 1.3382536530666791e-11, -3.3896802963044296e-13, 8.586062056093802e-15, -2.1748686983715528e-16, 5.509002826470406e-18, 0, 0, 0, 0]
-
+#coeff_list = [0.08333333333333333, -0.0013888888888888874, 3.3068783068782915e-05, -8.267195767195603e-07, 2.0876756987866386e-08, -5.284190138685735e-10, 1.3382536530666791e-11, -3.3896802963044296e-13, 8.586062056093802e-15, -2.1748686983715528e-16, 5.509002826470406e-18, 0, 0, 0, 0]
+coeff_list = [0]*15
 # coeff implements \frac{2k+1}{2(2k+3)!} - \sum_{j=0}^{k-1} \frac{a_j}{(2k+1-2j)!} 
 # implement 
 # j is here j-1 to include the first term
@@ -33,32 +33,41 @@ coeff_list = [0.08333333333333333, -0.0013888888888888874, 3.3068783068782915e-0
 
 # write now it prints out every part of a fraction to verify
 
-def sum(k,j): 
+def sum(k,verbose):
+     return sum_recursion(k,0,verbose)
+
+def sum_recursion(k,j,verbose): 
      if (j==k):
           return 0
      aux = coeff_list[j]/factorial(2*k+1-2*j)
-     print(f"   sum k:{k} j:{j}= {aux}")
-     return aux + sum(k,j+1)
+     if(verbose):
+          print(f"   sum k:{k} j:{j}= {aux}")
+     return aux + sum_recursion(k,j+1)
 
 
-def coeff(k, k_index) :
+def coeff(k):
+     return coeff_recursion(k,0, false)
+
+def coeff_recursion(k, k_index, verbose) :
      # when greater is than k than it should terminate
      if (k_index>k):
           return 0
      if (k_index==0): 
           coeff_list[0] = a_0
-          return coeff(k, k_index+1) 
+          return coeff_recursion(k, k_index+1,verbose) 
 
      aux1 =  (2*k_index+1)/(2 * factorial(2*k_index+3)) 
-     print(f"part 1 of k: {k_index} = {aux1}")
+     if(verbose):
+          print(f"part 1 of k: {k_index} = {aux1}")
 
-     aux2 = sum(k_index,0)
-     print(f"part 2 of k: {k_index} = {aux2}")
+     aux2 = sum(k_index)
+     if(verbose): 
+          print(f"part 2 of k: {k_index} = {aux2}")
      coeff_list[k_index]=aux1 - aux2
-     return coeff(k,k_index+1) 
+     return coeff_recursion(k,k_index+1, verbose) 
 
-#print(coeff(10,0))
-#print(coeff_list)
+print(coeff(10))
+print(coeff_list)
 
 
 
@@ -90,9 +99,9 @@ def bose(n, index,verbose):
 
 #bose(1,0) und bose(2,0) funktionieren nicht!! 
 f= f_ref
-print("bose(11,0,true): Laurent series of Bose function with 3 terms")
-h = bose(11,0,true)
-print(f"Bose_approx: {h}")
+#print("bose(11,0,true): Laurent series of Bose function with 3 terms")
+#h = bose(11,0,true)
+#print(f"Bose_approx: {h}")
 # h = 1/x
 # print(h)
 # diff_list= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
