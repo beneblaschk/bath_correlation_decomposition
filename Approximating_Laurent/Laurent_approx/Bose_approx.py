a35bb7c3a1e82aec78ea2f534c6d0dedb76e6407
Benedict Blaschko <ge93kux@mytum.de>
2024-07-22 10:58:48 +0200

    changed the index, so n: number of terms, index starts at zero, and when they are the same, it breaks out

--- Approximating_Laurent/Laurent_approx/Bose_approx.py
+++ Approximating_Laurent/Laurent_approx/Bose_approx.py
@@ -80,8 +80,6 @@ def bose(n):
 
 def bose_recursion(n, index,verbose): 
     if(index==n): 
-         if(index==0):
-              return a_0
          return 0
     
     # could be more compact, but probably not necessary
@@ -108,7 +106,7 @@ def bose_recursion(n, index,verbose):
 #bose(1,0) und bose(2,0) funktionieren nicht!! 
 f= f_ref
 #print("bose(11,0,true): Laurent series of Bose function with 3 terms")
-h = bose_recursion(11,0,true)
+h = bose_recursion(10,0,true)
 print(h)
 #print(f"Bose_approx: {h}")
 # h = 1/x
