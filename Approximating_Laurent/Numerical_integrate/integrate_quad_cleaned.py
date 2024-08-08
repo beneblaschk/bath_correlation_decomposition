d0b41b69d93cc43d933884d91784905d21101a9d
Benedict Blaschko <ge93kux@mytum.de>
2024-08-08 16:37:15 +0200

    safe

--- Approximating_Laurent/Numerical_integrate/integrate_quad_cleaned.py
+++ Approximating_Laurent/Numerical_integrate/integrate_quad_cleaned.py
@@ -16,8 +16,8 @@ bath_front_faktor = 1/(numpy.pi)
 
 
 #for the plot: 
-number_of_steps= 10
-step_size = 1
+number_of_steps= 100
+step_size = 0.1
 
 start_table_string ="\\begin{array}{|c|c|}\\hline\\tau & \\alpha\\\\"
 end_table_string = "\\hline\\end{array}"
@@ -44,6 +44,16 @@ def plot(function_to_plot) :
     print(end_table_string)
     return 0
 
+def bath_integralfunction_closed_fixed(w):
+        return spectral_density(w) * (bose_closed(w)-1) * numpy.exp((- (0+1j)*w*2))
 
-#print(bath_closed(1))
+
 plot(bath_closed)
+
+# result_1 = integrate.quad(bath_integralfunction_closed_fixed, lower_integral_limit, 0-distance_to_signularity)
+# result_2 = integrate.quad(bath_integralfunction_closed_fixed, 0+distance_to_signularity, upper_integral_limit)
+# print(bath_front_faktor *(result_1[0]+result_2[0]))
+
+
+#-> es sollte eigentlich nicht an dem argument ligen! 
+# das kann man jetzt noch weiter argumentieren, aber ich bin mir nicht so sicher! 
