e25a732c0897b067b3a7a3b6f70d710b73f8fcb3
Benedict Blaschko <ge93kux@mytum.de>
2024-08-16 11:03:44 +0200

    # Iterations über die Graphen gehen von 0 bis number_of_grahs (zb 4) # Das gilt für label, alpha_werte, alpha_values into the data_set # adding the number_of_datapoints -> einheitlich wieviele tau werte es geben soll # number_of_data_points are relevant for: creating empty data_set, adding alphavalues to the data set

--- Approximating_Laurent/Pipeline/formatter.py
+++ Approximating_Laurent/Pipeline/formatter.py
@@ -12,9 +12,10 @@ import universal_plot
 # Parameter compare
 
 #commit: 
-# added verbose version with print output
-# color and label is still inside this formatter, color is overriden in universal_plot
-# added number of graphs -> parameter unterschiedliche configurationen 
+# Iterations über die Graphen gehen von 0 bis number_of_grahs (zb 4)
+# Das gilt für label, alpha_werte, alpha_values into the data_set 
+# adding the number_of_datapoints -> einheitlich wieviele tau werte es geben soll
+# number_of_data_points are relevant for: creating empty data_set, adding alphavalues to the data set
 
 
 def format(compare,spectral_density,bose,integral):
@@ -26,8 +27,8 @@ def format(compare,spectral_density,bose,integral):
     integral: residual - numerical
     """
     tau_range = [0]*3 
-    tau_range[0] = 0.1 
-    tau_range[1] = 2.0
+    tau_range[0] = 0.0
+    tau_range[1] = 1.5
     tau_range[2] = 0.1
 
     parameter_range = []
@@ -38,7 +39,7 @@ def format(compare,spectral_density,bose,integral):
     parameter_range.append([])
     parameter_range[1] = [0]*3
 
-    parameter_range[1][0] = 1  # gamma =0,10.0, 0.1
+    parameter_range[1][0] = 3  # gamma =0,10.0, 0.1
     parameter_range[1][1] = 5
     parameter_range[1][2] = 0.5
 
@@ -48,7 +49,7 @@ def format(compare,spectral_density,bose,integral):
 
 def format_advanced_parameters(compare,spectral_density,bose,integral,tau_range, parameter_range):
 
-    format_advanced_parameter(compare, spectral_density, bose, integral, tau_range, parameter_range, True)
+    format_advanced_parameter(compare, spectral_density, bose, integral, tau_range, parameter_range, False)
 
 
 def format_advanced_parameter(compare, spectral_density, bose, integral, tau_range, parameter_range, verbose):
@@ -94,6 +95,10 @@ def format_advanced_parameter(compare, spectral_density, bose, integral, tau_ran
 
 
     number_of_graphs = int((gamma_max-gamma_start)/gamma_steps)
+
+    # es wird wohl immer der letzte punkt rausgelassen, 
+    #TODO fix the boundaries
+    number_of_datapoints = int((tau_end-tau_start)/tau_step_size)-1
     if verbose:
         print(f"resulting in {number_of_graphs} graphs")
 
@@ -107,25 +112,33 @@ def format_advanced_parameter(compare, spectral_density, bose, integral, tau_ran
 
 
     label = [] 
-
-    for i in range(gamma_start,number_of_graphs):
+    # nur label
+    for i in range(0,number_of_graphs):
         label.append([f"{eta}_{float(i*gamma_steps+gamma_start)}",''])
     #TODO: color is managed in universal plot -> but also here -> left empty 
 
     # hier werden die tau-Werte eingespeist
     data_set = [[round(i, 1)] for i in np.arange(tau_start,tau_end-0.1, tau_step_size)]
     #TODO: Warum ist hier -0.1 bei tau end?
+    if verbose:
+        print(f"empty data_set: \n {data_set}")
 
 
     #Hier werden die alpha Werte eingespeist
     alpha_values = [0]*number_of_graphs
-    for g in range (gamma_start,number_of_graphs):
-        alpha_values[g] = calculate_alpha_values[0](1,g*gamma_steps,tau_range)
+    for g in range (0,number_of_graphs):
+        alpha_values[g] = calculate_alpha_values[0](1,g*gamma_steps+gamma_start,tau_range)
+        if verbose :
+            print(f"alpha_value[{g}][2] = {alpha_values[g][2]} for this gamma: {g*gamma_steps+gamma_start}")
 
-    # Hier werden die alpha werte alle zu einer Datenstruktur kombiniert 
+    if verbose: 
+        print("alpha_values")
+        print(alpha_values)
 
-    for i in range(0,len(data_set)):
-        for g in range(gamma_start,number_of_graphs):
+
+    # Hier werden die alpha werte an das leeere Data_set angehängt, sodass zu jedem tau wert der richtige alphawert passt 
+    for i in range(0,number_of_datapoints):
+        for g in range(0,number_of_graphs):
             data_set[i].append(alpha_values[g][i])
 
     #show data
