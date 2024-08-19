0f301094106a0ac4611109c65f5d10f9e5b1c77e
Benedict Blaschko <ge93kux@mytum.de>
2024-08-19 09:38:53 +0200

    # deleted plot info, step size and so on # deleted bath_closed_integral with fixed spectral density # deleted bath_closed with fixed spectral density # deleted bath_closed_tau_set with fixed sd # deleted plotter

--- Approximating_Laurent/Pipeline/integrate_quad_cleaned.py
+++ Approximating_Laurent/Pipeline/integrate_quad_cleaned.py
@@ -6,7 +6,11 @@ import sympy
 
 #import Bose_approx
 #commit log:
-# now we want to do closed and approxed together! 
+# deleted plot info, step size and so on 
+# deleted bath_closed_integral with fixed spectral density 
+# deleted bath_closed with fixed spectral density 
+# deleted bath_closed_tau_set with fixed sd
+# deleted plotter 
 
 x, y = sympy.symbols('x y')
 #for the integral
@@ -16,10 +20,6 @@ distance_to_signularity = 0.01
 bath_front_faktor = 1/(numpy.pi)
 #bath_front_faktor = 1
 
-
-#for the plot: 
-number_of_steps= 100
-step_size = 0.1
 
 start_table_string ="\\begin{array}{|c|c|}\\hline\\tau & \\alpha\\\\"
 end_table_string = "\\hline\\end{array}"
@@ -41,16 +41,8 @@ def spectral_density(x):
 def bose_closed(x): 
         return 1/(1-numpy.exp(-x))
 
-def bath_integralfunction_closed(w,t):
-        return spectral_density(w) * (bose_closed(w)-1) * numpy.exp((- (0+1j)*w*t))
-
 def bath_integralfunction_closed_sd_select(w,t,sd):
         return sd(w) * (bose_closed(w)-1) * numpy.exp((- (0+1j)*w*t))
-
-def bath_closed(t) :
-   result_1 = integrate.quad(bath_integralfunction_closed, lower_integral_limit, 0-distance_to_signularity,args=t)
-   result_2 = integrate.quad(bath_integralfunction_closed, 0+distance_to_signularity, upper_integral_limit,args=t)
-   return bath_front_faktor *(result_1[0]+result_2[0])
 
 def bath_closed_sd_select(t, sd): 
         result_1 = integrate.quad(bath_integralfunction_closed_sd_select, lower_integral_limit, 0-distance_to_signularity,args=(t,sd))
@@ -59,11 +51,6 @@ def bath_closed_sd_select(t, sd):
         # i need to check wether the arguments are correctly transmitted
         
         return bath_front_faktor *(result_1[0]+result_2[0])
-
-
-def bath_closed_tau_set(eta,gamma,tau_range):
-      t_values = numpy.arange(tau_range[0], tau_range[1],tau_range[2])
-      return [bath_closed(t) for t in t_values]
 
 def bath_closed_tau_set_sd_select(sd,tau_range):
       """
@@ -83,15 +70,5 @@ def bath_closed_tau_set_sd_select(sd,tau_range):
       return [bath_closed_sd_select(t,sd) for t in t_values]
 
 
-
-def plot(function_to_plot) : 
-    print(start_table_string)
-    for i in range (1,number_of_steps):
-        print(f"{float(i*step_size):.2f} & {float(function_to_plot(i*step_size)):.4f}\\\\")
-    print(end_table_string)
-    return 0
-
-def bath_integralfunction_closed_fixed(w):
-        return spectral_density(w) * (bose_closed(w)-1) * numpy.exp((- (0+1j)*w*2))
 if __name__ == "__main__":
-        plot(bath_closed)
+                print("main")
